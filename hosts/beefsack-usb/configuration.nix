c6271258274840ea6030b8d0b2728f1f0715edf6
Michael Alexander <beefsack@gmail.com>
2024-05-17 10:36:24 +1000

    Use volatile storage for journald for beefsack-usb

--- hosts/beefsack-usb/configuration.nix
+++ hosts/beefsack-usb/configuration.nix
@@ -2,4 +2,12 @@
 
 {
   networking.hostName = "beefsack-usb"; # Define your hostname.
+
+  # Use volatile storage for journald to reduce disk access
+  environment.etc = {
+    "systemd/journald.conf.d/99-storage.conf".text = ''
+      [Journal]
+      Storage=volatile
+    '';
+  };
 }
