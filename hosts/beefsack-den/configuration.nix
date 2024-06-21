2b6af735d97c65f061c78a77be0df586e552c333
Michael Alexander <beefsack@gmail.com>
2024-06-22 09:17:45 +1000

    Flake update and remove opengl

--- hosts/beefsack-den/configuration.nix
+++ hosts/beefsack-den/configuration.nix
@@ -5,13 +5,6 @@
 
   # NVIDIA
 
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
 
