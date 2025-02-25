a44dc68fc81c132e47fedbad1b566ff34e266f76
Michael Alexander <beefsack@gmail.com>
2025-02-25 11:05:15 +1100

    Flake update

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -4,41 +4,43 @@
   # vscode
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      dart-code.flutter
-      dracula-theme.theme-dracula
-      eamodio.gitlens
-      github.copilot
-      golang.go
-      jnoortheen.nix-ide
-      mkhl.direnv
-      ms-azuretools.vscode-docker
-      #rust-lang.rust-analyzer
-      tamasfe.even-better-toml
-      vscodevim.vim
-    ];
-    userSettings = {
-      "telemetry.telemetryLevel" = "off";
-      "workbench.startupEditor" = "none";
-      "workbench.colorTheme" = "Dracula Theme";
-      "vim.useSystemClipboard" = true;
-      "window.titleBarStyle" = "custom";
-      "editor.fontFamily" = "SauceCodePro Nerd Font Mono";
-      "editor.fontWeight" = 500;
-      "editor.fontSize" = 16;
-      "editor.rulers" = [ 80 ];
-      "vim.handleKeys" = {
-        "<C-p>" = false;
-      };
-      "[rust]" = {
-        "editor.rulers" = [ 120 ];
-        "editor.formatOnSave" = true;
-      };
-      "update.mode" = "none";
-      "[dart]" = {
-        "editor.formatOnSave" = true;
-        "editor.formatOnType" = true;
-        "editor.selectionHighlight" = false;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        dart-code.flutter
+        dracula-theme.theme-dracula
+        eamodio.gitlens
+        github.copilot
+        golang.go
+        jnoortheen.nix-ide
+        mkhl.direnv
+        ms-azuretools.vscode-docker
+        rust-lang.rust-analyzer
+        tamasfe.even-better-toml
+        vscodevim.vim
+      ];
+      userSettings = {
+        "telemetry.telemetryLevel" = "off";
+        "workbench.startupEditor" = "none";
+        "workbench.colorTheme" = "Dracula Theme";
+        "vim.useSystemClipboard" = true;
+        "window.titleBarStyle" = "custom";
+        "editor.fontFamily" = "SauceCodePro Nerd Font Mono";
+        "editor.fontWeight" = 500;
+        "editor.fontSize" = 16;
+        "editor.rulers" = [ 80 ];
+        "vim.handleKeys" = {
+          "<C-p>" = false;
+        };
+        "[rust]" = {
+          "editor.rulers" = [ 120 ];
+          "editor.formatOnSave" = true;
+        };
+        "update.mode" = "none";
+        "[dart]" = {
+          "editor.formatOnSave" = true;
+          "editor.formatOnType" = true;
+          "editor.selectionHighlight" = false;
+        };
       };
     };
   };
