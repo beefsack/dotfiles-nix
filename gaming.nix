30984ea244e2c469defb055935defb83f455900f
Michael Alexander <beefsack@gmail.com>
2025-12-09 17:52:46 +1100

    Add GE-Proton

--- gaming.nix
+++ gaming.nix
@@ -16,6 +16,9 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 
   programs.gamescope = {
