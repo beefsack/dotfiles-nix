e331f7e201c4d4ef3ebec1a7c7402ccecb86c9e6
Michael Alexander <beefsack@gmail.com>
2026-01-24 14:26:44 +1100

    Migrate fish config to home manager options

--- modules/home/terminal.nix
+++ modules/home/terminal.nix
@@ -1,7 +1,14 @@
 { ... }:
 
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = "set fish_greeting";
+  };
+
+  programs.direnv.enable = true;
+
+  programs.starship.enable = true;
 
   programs.ghostty = {
     enable = true;
@@ -36,8 +43,4 @@
     };
   };
 
-  programs.starship.enable = true;
-  home.file.".config/fish/conf.d/greeting.fish".source = ../../.config/fish/conf.d/greeting.fish;
-  home.file.".config/fish/conf.d/starship.fish".source = ../../.config/fish/conf.d/starship.fish;
-  home.file.".config/fish/conf.d/direnv.fish".source = ../../.config/fish/conf.d/direnv.fish;
 }
