ed71b1c807052fb1aa066a1671d8d96e7b176566
Michael Alexander <beefsack@gmail.com>
2026-03-28 21:20:28 +1100

    Change git signing format to new default

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -4,6 +4,9 @@
   # basic configuration of git, please change to your own
   programs.git = {
     enable = true;
+    signing = {
+      format = null;
+    };
     settings = {
       user = {
         name = "Michael Alexander";
